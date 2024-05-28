e3255e8b92a11709f5a66bb9d651f865ac68b37d
Navin joshi <115449678+Navinjoshi1301@users.noreply.github.com>
2024-05-28 17:26:21 +0530

    Update TodoRepository.java

--- src/main/java/com/example/springbootjava/repository/TodoRepository.java
+++ src/main/java/com/example/springbootjava/repository/TodoRepository.java
@@ -2,6 +2,4 @@ package com.example.springbootjava.repository;
 
 import com.example.springbootjava.domain.Todo;
 import org.springframework.data.repository.CrudRepository;
-
-
 public interface TodoRepository extends CrudRepository<Todo, Long> { }
